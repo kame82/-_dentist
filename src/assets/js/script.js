8589dd0279ea32e9667a3eb6c3acd840ad1d95a4
kaoru <kametyaso@gmail.com>
2023-12-29 23:20:52 +0900

    sp update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,11 +2,20 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
+  speed: 1500,
+  effect: "fade", //fade effect
+  fadeEffect: {
+    //追加
+    crossFade: true, //追加 (スライドの重なりを解消)
+  },
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
   },
 
   // Navigation arrows
